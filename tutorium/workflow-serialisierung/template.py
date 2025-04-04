9f432dad219095a70093daaf5bdc1efe9c8e0e04
Albert Weichselbraun <albert@weichselbraun.net>
2025-04-04 07:11:40 +0200

    add: database template.

---
+++ tutorium/workflow-serialisierung/template.py
@@ -0,0 +1,44 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, String, Integer, Float
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_engine('sqlite:///newsletter.db', echo=True)
+
+# Basisklasse für die Datenklassen
+Base = declarative_base()
+
+class Entry(Base):
+    __tablename__ = 'beispiels_eintrag'
+
+    key = Column(String, primary_key=True)
+    value1 = Column(String)
+    value2 = Column(Integer)
+    value3 = Column(Float)
+
+    def __repr__(self):
+        return f"<Entry(email={self.key}, name={self.value1}, firma={self.value2}, kanton={self.value3})>"
+
+Base.metadata.create_all(engine)
+
+
+def add_entry(key, value1, value2, value3):
+    global engine
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    entry = Entry(key, value1, value2, value3)
+    session.add(entry)
+    session.commit()
+
+def get_entries():
+    global engine
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session.query(Entry).order_by(Entry.key)
+
+def get_entry_for_key(key):
+    global engine
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session.query(Entry).filter(Entry.key=key).first()
